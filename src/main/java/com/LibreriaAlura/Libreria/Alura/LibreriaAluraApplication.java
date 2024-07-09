6c4a7fdf812d4bd4d2bd12537b910d61692d35f7
Gabriela <gabb.pardo@gmail.com>
2024-07-09 11:45:36 -0300

    Arrancamos

---
+++ src/main/java/com/LibreriaAlura/Libreria/Alura/LibreriaAluraApplication.java
@@ -0,0 +1,13 @@
+package com.LibreriaAlura.Libreria.Alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaAluraApplication.class, args);
+	}
+
+}
